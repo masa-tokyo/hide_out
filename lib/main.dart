72875e5f71241f6ff8ed742722480d819e2f3d8d
masaki <masakisato.japan@gmail.com>
2025-04-27 17:15:48 +0900

    remove all files
    just removed the entire dart files and dependencis to just run the project. Revert it later.

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,14 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:hide_out/di/providers.dart';
-import 'package:hide_out/models/tracking.dart';
-import 'package:hide_out/view/root_screen.dart';
-import 'package:hide_out/utils/functions.dart';
-import 'package:hide_out/utils/style.dart';
-import 'package:hide_out/view/login/terms_and_conditions.dart';
-import 'package:url_strategy/url_strategy.dart';
-import 'package:provider/provider.dart';
 
-import 'view/login/privacy_policy_screen.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await _initializeFirebase();
-
-  // remove '#' from url
-  setPathUrlStrategy();
-  runApp(
-    MultiProvider(
-      providers: globalProviders,
-      child: MyApp(),
-    ),
-  );
-
-}
-
-
-
-Future<void> _initializeFirebase() async {
-  await Firebase.initializeApp();
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-
-    return GestureDetector(
-      onTap: () => unFocusKeyboard(context: context),
-      child: MaterialApp(
-        title: "HideOut",
-        debugShowCheckedModeBanner: false,
-        theme: lightTheme,
-        darkTheme: darkTheme,
-        themeMode: ThemeMode.dark,
-        navigatorObservers: [
-          Tracking().getPageViewObserver(),
-        ],
-        initialRoute: RootScreen.routeName,
-        routes: {
-          RootScreen.routeName: (_) => RootScreen(),
-          PrivacyPolicyScreen.routeName: (_) => PrivacyPolicyScreen(),
-          TermsAndConditionsScreen.routeName: (_) => TermsAndConditionsScreen(),
-        },
-      ),
-    );
+    return MaterialApp(home: Container());
   }
 }
-
